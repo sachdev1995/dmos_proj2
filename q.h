d08863376ab4e76bb2d541e5a2cd0db9a9c825e5
sachdev1995 <sachdev1995@asu.edu>
2019-10-05 11:07:11 -0700

    test

--- q.h
+++ q.h
@@ -1,30 +1,26 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "TCB.h"
 
-typedef struct QueueItems
-{
-	int data;
-	struct QueueItems *next;
-	struct QueueItems *prev;
-}Queue;
+
 
 int count = 0 ;
 
 
 
-Queue *newQueue()
+TCB_t *newQueue()
 {
 	
-	Queue *q = NULL;
+	TCB_t *q = NULL;
 	return q;
 };
 
 
 
-Queue *NewItem()
+TCB_t *NewItem()
 {
-	Queue *q = (Queue *)malloc (sizeof(Queue ));
-	q->data = ++count;
+	TCB_t *q = (TCB_t *)malloc (sizeof(TCB_t ));
+	//q->thread_id = ++count;
 	q->next = NULL;
 	q->prev = NULL;
 	return q;
@@ -32,7 +28,7 @@ Queue *NewItem()
 
 
 
-void AddQueue(Queue **headPtr, Queue *item )
+void AddQueue(TCB_t **headPtr, TCB_t *item )
 {
 	if(*headPtr == NULL)
 	{
@@ -42,7 +38,7 @@ void AddQueue(Queue **headPtr, Queue *item )
 		return;
 	}
 
-	Queue *temp = (*headPtr)->prev;
+	TCB_t *temp = (*headPtr)->prev;
 	temp->next = item;
 	item->prev = temp;
 	item->next = *headPtr;
@@ -52,14 +48,14 @@ void AddQueue(Queue **headPtr, Queue *item )
 
 }
 
-Queue *DelQueue(Queue **headPtr)
+TCB_t *DelQueue(TCB_t **headPtr)
 {
 	if(*headPtr == NULL)
 	{
 		printf("queue is empty, can't delete anything\n");
 		return NULL;
 	}
-	Queue *deleted;
+	TCB_t *deleted;
 	if(( (((*headPtr)->next) == (*headPtr))) &&  (((*headPtr)->prev) == (*headPtr)))
 	{
 		 deleted = *headPtr;
@@ -70,7 +66,7 @@ Queue *DelQueue(Queue **headPtr)
 	}
 	else
 	{
-		Queue *temp = (*headPtr)->prev;
+		TCB_t *temp = (*headPtr)->prev;
 		temp->next = (*headPtr)->next;
 		(*headPtr)->next->prev = temp;
 		deleted = *headPtr;
@@ -81,9 +77,9 @@ Queue *DelQueue(Queue **headPtr)
 
 
 
-void printAll(Queue *headPtr)
+void printAll(TCB_t *headPtr)
 {
-	Queue *temp = headPtr;
+	TCB_t *temp = headPtr;
 
 	if(temp == NULL)
 	{
@@ -91,24 +87,24 @@ void printAll(Queue *headPtr)
 		return ;
 	}
 	
-	printf("Queue data is \n");
-	printf("first data is \n%d\n",temp->data);
+	printf("Queue thread_id is \n");
+	printf("first thread_id is \n%d\n",temp->thread_id);
 	temp = temp->next;
 	while(headPtr != temp)
 	{
-		printf("%d\n",temp->data);
+		printf("%d\n",temp->thread_id);
 		temp = temp->next;
 	}
 	printf("backwards\n");
 	
 	temp = temp->prev;
-	printf("first data is \n");
+	printf("first thread_id is \n");
 	while(headPtr != temp)
 	{
-		printf("%d\n",temp->data);
+		printf("%d\n",temp->thread_id);
 		temp = temp->prev;
 	}
-	printf("%d\n",temp->data);
+	printf("%d\n",temp->thread_id);
 	
 
 }
--- thread_test.c
+++ thread_test.c
@@ -1,36 +1,74 @@
-#include "q.h"
+#include "threads.h"
+
+int global_counter = 0;
+
+void funct1(void)
+{
+	int local_counter = 0;
+	while(1)
+	{
+		printf("in func1 global_counter = %d, local_counter = %d\n",global_counter++,local_counter++);
+		yield();
+	}
+}
+
+void funct2(void)
+{
+	int local_counter = 0;
+	while(1)
+	{
+		printf("in func2 global_counter = %d, local_counter = %d\n",global_counter++,local_counter++);
+		yield();
+	}
+}
+
 
 int main()
 {
 	printf("in main\n");
 
-	Queue *head = newQueue();
+	start_thread(funct1);
+	start_thread(funct2);
+	start_thread(funct1);
+
+	run();
+
+	/*
+
+	TCB_t *head = newQueue();
 	
-	Queue *item = NewItem();
+	TCB_t *item = NewItem();
 	
 	AddQueue(&head,item);
-	Queue *item1 = NewItem();
+	TCB_t *item1 = NewItem();
 	AddQueue(&head,item1);
-	Queue *item2 = NewItem();
+	TCB_t *item2 = NewItem();
 	AddQueue(&head,item2);
 	printAll(head);
-	Queue *item3 = DelQueue(&head);
-	if (item3!= NULL) printf("deleted item is %d\n",item3->data);
+	TCB_t *item3 = DelQueue(&head);
+	if (item3!= NULL) printf("deleted item is %d\n",item3->thread_id);
 	
-	Queue *item4 = DelQueue(&head);
-	if (item4!= NULL) printf("deleted item is %d\n",item4->data);
+	TCB_t *item4 = DelQueue(&head);
+	if (item4!= NULL) printf("deleted item is %d\n",item4->thread_id);
 	
-	Queue *item5 = DelQueue(&head);
-	if (item5!= NULL) printf("deleted item is %d\n",item5->data);
+	TCB_t *item5 = DelQueue(&head);
+	if (item5!= NULL) printf("deleted item is %d\n",item5->thread_id);
 	
-	Queue *item6 = DelQueue(&head);
-	if (item6!= NULL) printf("deleted item is %d\n",item6->data);
+	TCB_t *item6 = DelQueue(&head);
+	if (item6!= NULL) printf("deleted item is %d\n",item6->thread_id);
 
 	printAll(head);
 	AddQueue(&head,item2);
 	AddQueue(&head,item1);
-	Queue *item7 = DelQueue(&head);
+	TCB_t *item7 = DelQueue(&head);
 	printAll(head);
+	 DelQueue(&head);
+	printAll(head);
+	*/
+
+
+
+
 
 	return 0;
 }
